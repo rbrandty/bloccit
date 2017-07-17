ff4d48004ce4518a5a04c9873c86ad2cb7f2afeb
Rachel Brandt <rachel.brandt@gmail.com>
2017-07-17 17:17:52 -0400

    checkpoint16 file changes

--- config/application.rb
+++ config/application.rb
@@ -19,11 +19,13 @@ Bundler.require(*Rails.groups)
 module Bloccit
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-   config.load_defaults 5.1
+    config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths << File.join(config.root, "lib")
+
 
     # Don't generate system test files.
     config.generators.system_tests = nil
