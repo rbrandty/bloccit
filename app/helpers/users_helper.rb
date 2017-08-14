ccee577eb7614dca7eabe7b171b2cf02fcf8e1e8
Rachel Brandt <rachel.brandt@gmail.com>
2017-08-14 16:14:01 -0400

    modify files for assignment21 - Rails public profiles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+
+  def user_has_posts?(user)
+    user.posts == true
+  end
+
+  def user_has_comments?(user)
+    user.comments == true
+  end
+
+  def favorite_for(post)
+     favorites.where(post_id: post.id).first
+  end
+
 end
